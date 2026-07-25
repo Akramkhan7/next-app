509fb8a6241c6a9b2695984fa5194047e51e8247
Akramkhan7 <m.akramkhan6690@gmail.com>
2026-07-25 16:24:27 +0530

    Done

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,10 @@
+import Image from "next/image";
+
 export default function Home() {
-  return <h1>Welcome to the Products Store</h1>;
+  return (
+    <>
+      <h1>Welcome to the Products Store</h1>
+      <Image src="/images/img.png" width={500} height={500}></Image>
+    </>
+  );
 }
